354ebbe190bf5e2a93b61d0c708c05e9bed0488f
Selçuk <selcukcan74@gmail.com>
2025-11-26 20:01:13 +0300

    verify system update

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"88f960983fb5279deaa9bafc3586729d03b5bc
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1797284579" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4024262041" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
